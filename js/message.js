a45b6aff34ad768b83c963c36b2bc3ebb1ce0b82
HongyiZeng <277382367@qq.com>
2019-06-07 10:49:19 +0800

    test

--- js/message.js
+++ js/message.js
@@ -1,7 +1,7 @@
 window.onload=function(){
 	code();
 	var fm=document.getElementsByTagName('form')[0];
-	fm.onsubmit=function{
+	fm.onsubmit=function(){
 		//验证码
 		if (fm.code.value.length != 4) {
 			alert('验证码不正确');
